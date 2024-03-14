708706c0e1d8db45b8200da01ef15e74f78de0ed
zyr17 <jzjqz17@gmail.com>
2024-03-14 22:08:11 +0800

    add debug code

--- src/main.cpp
+++ src/main.cpp
@@ -55,11 +55,13 @@ namespace MAIN{
             std::cout << "open success: " << dataprefix + "data/" + source << std::endl;
             dirent *entry;
             while (entry = readdir(dirptr)){
+                std::cout << "entry id: " << entry -> d_name << std::endl;
                 ids.push_back(entry -> d_name);
                 if (*ids.rbegin() == "." || *ids.rbegin() == ".." || *ids.rbegin() == "0")
                     ids.pop_back();
             }
             closedir(dirptr);
+            std::cout << "close success: " << dataprefix + "data/" + source << std::endl;
         #else
             Out::cout << I18N::get("MAIN", "MACROUNDEFINED") << '\n';
             PAUSEEXIT;
@@ -94,13 +96,16 @@ namespace MAIN{
         }
         config.Get("id", id);
         std::vector<std::string> ids = findid(Header::datafolderprefix, source, id);
+        std::cout << "ID: " << id << std::endl;
         if (!ids.size()){
             Out::cout << I18N::get("MISC", "ERROR") + I18N::get("MAIN", "CANTDATA/SRC/*") + source + I18N::get("MAIN", "CANTDATA/SRC/*AFT") + "\n";
             PAUSEEXIT;
             return 1;
         }
+        std::cout << "Try to print to Out" << std::endl;
         Out::cout << '\n';
         Out::cout << I18N::get("MAIN", "SRC") + I18N::get("MISC", "COLON") + source + "\n";
+        std::cout << "Try to print to Out Success" << std::endl;
         std::string outputid = id;
         #ifdef _WIN32
             outputid = Algo::UTF82GBK(outputid);
@@ -137,6 +142,7 @@ int main(int argc, char *argv[]){
     //Algo::testtenpai(); return 0;
 
     auto rcres = readconfig();
+    std::cout << "Read Config Result: " << rcres << std::endl;
     if (rcres == 1) return 0;
 
     if (argc > 1 && argv[1] == std::string("--tenhou-basedata")){
